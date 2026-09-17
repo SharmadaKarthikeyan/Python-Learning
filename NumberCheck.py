24a79cd50178b9ce250b99cf54b6e31881508101
SharmadaKarthikeyan <326131559+SharmadaKarthikeyan@users.noreply.github.com>
2026-09-17 20:39:16 +0530

    NumberCheck version

---
+++ NumberCheck.py
@@ -0,0 +1,9 @@
+a =  int(input("Enter a number: "))
+
+if a > 100:
+    print("Number is Greater than 100")
+elif a < 100:
+    print("Number is Less than 100")
+else:
+    print("Number is Equal to 100")
+    
