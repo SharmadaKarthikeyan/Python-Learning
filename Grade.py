3fa012192001c862ce12cbd5f9968cafc4c8881e
SharmadaKarthikeyan <326131559+SharmadaKarthikeyan@users.noreply.github.com>
2026-09-17 19:38:25 +0530

    Grade version

---
+++ Grade.py
@@ -0,0 +1,16 @@
+mark = int(input("Enter a mark: "))
+
+if mark >= 95:
+    print("Grade A")
+elif mark >= 85:
+    print("Grade B")
+elif mark >= 75:
+    print("Grade c")
+elif mark >= 50:
+    print("Grade D")
+else:
+    print("Grade E")
+
+
+          
+           
